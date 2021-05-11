87db4d3f3b0a4fcd6003b6189a3a912d07a950e7
makersmelx <makersmelx@gmail.com>
2021-05-11 09:19:52 +0800

    chore(*): doc update and remove useless code

--- GradeFromJOJ/GradeFromJOJ.py
+++ GradeFromJOJ/GradeFromJOJ.py
@@ -20,6 +20,7 @@ def joj_score_to_canvas_score(_student):
         exit(1)
     for i in range(len(settings.csv_score_column)):
         _column_grade = _student[settings.csv_score_column[i]]
+        # todo: complete non number detection
         if _column_grade == '-':
             _column_grade = 0
         _score += float(_column_grade) * settings.weight[i]
@@ -30,42 +31,10 @@ def joj_score_to_canvas_score(_student):
         _score += _adjust
         print("Extra adjust: {}".format(_adjust))
 
-    # bonus or deduction
-    bonus_second = 0
-    if settings.has_bonus:
-        bonus_second = (settings.bonus_date - settings.start_date).total_seconds()
-    due_second = (settings.due_date - settings.start_date).total_seconds()
-
-    # calculate final submission timestamp
-    if len(settings.csv_last_submission_timestamp_column) > 0:
-        available_second = due_second + 3600 * 24 * len(settings.csv_last_submission_timestamp_column)
-        last_submissions = []
-        for item in settings.csv_last_submission_timestamp_column:
-            if _student[item] != '-':
-                last_submissions.append(int(_student[item]))
-        finish_second = max(last_submissions) if len(last_submissions) > 0 else available_second
-    else:
-        finish_second = due_second
-
-    finish_delta = timedelta(seconds=finish_second)
-    finish_datetime = settings.start_date + finish_delta
-    print('Submission at: {}'.format(finish_datetime))
-
     print('Raw score: {}'.format(_score))
     if settings.has_bonus and finish_second <= bonus_second:
         _score += _score * settings.bonus_portion
-    elif finish_second > due_second:
-        one_day_in_seconds = 3600 * 24
-        max_late_day = len(settings.late_deduction)
-        if max_late_day > 0:
-            if finish_second > due_second + max_late_day * one_day_in_seconds:
-                _score = 0
-            else:
-                for i in range(max_late_day):
-                    if finish_second > due_second + i * one_day_in_seconds:
-                        _score -= settings.late_deduction[i] * settings.full_points
-                    else:
-                        break
+
     print('Now score: {}'.format(_score))
 
     if _student[settings.csv_sjtu_id_column] in settings.direct_reassign:
--- GradeFromJOJ/settings.py
+++ GradeFromJOJ/settings.py
@@ -6,18 +6,16 @@ by makersmelx <makersmelx@gmail.com>
 from datetime import datetime
 
 """
-Get them on Canvas (look at the website url, you will know the id)
+Get them on Canvas (look at the url of the assignment, you will know the id)
 """
-
 # REQUIRED FIELDS
 course_id = 0
 
 # REQUIRED FIELDS
 assignment_id = 0
-
-# Full points of this assignment
-# REQUIRED FIELDS
-full_points = 100
+"""
+Get them on Canvas (look at the url of the assignment, you will know the id)
+"""
 
 """
 CSV
@@ -25,7 +23,7 @@ Notice: all the column starts from 0
 """
 # notice the csv should be exported from joj notice that this python script will be started in the root directory,
 # write the path of the csv file according to GiteaCanvasHelper root directory
-# REQUIRED PATH
+# REQUIRED
 csv_path = ''
 
 # the row that records the first student's scores
@@ -42,36 +40,10 @@ csv_sjtu_id_column = 2
 write all the column index (of the csv, start from 0) of the score that you would like to include
 remember that for each column index please give them a weight in the second list
 """
-# example: csv_score_column = [7, 8], weight = [0.1, 0.2], grade for a student is ``row[7] * 0.1 + row[8] * 0.2``
+# example: csv_score_column = [7, 8], weight = [0.1, 0.2], suppose that all data in one row is a list called row.
+# Grade for this student is ``row[7] * 0.1 + row[8] * 0.2``
 csv_score_column = [3]
 weight = [0.1]
-
-"""
-How to decide the final submission time? Suppose you will define the time of the latest submission of three 
-problems as the final submission time of this assignment, write down the column of the used time (in seconds) of 
-three problems here 
-"""
-# Usually no need to fill this field if you are using from JOJ
-csv_last_submission_timestamp_column = []
-
-"""
-Date settings
-"""
-# Usually no need to fill these field if you are using from JOJ
-
-start_date = datetime(2020, 9, 25)
-
-due_date = datetime(2020, 10, 11, 23, 59, 59)
-# late deduction is multiplied to the full point of this assignment rather than the current score rather than,
-# and then deducted from the current score
-# late_deduction = [0.1, 0.1, 0.1] means 10% deduction per day, and 0 pt for more than three days
-late_deduction = []
-
-# Bonus settings
-has_bonus = False
-bonus_date = datetime(2020, 10, 4, 23, 59, 59)
-# bonus portion is multiplied to the current score rather than the full point of this assignment
-bonus_portion = 0.1
 
 """
 adjust to canvas score
@@ -85,5 +57,36 @@ extra_adjust = {
 
 # direct reassign point on Canvas to a student, will do this after all the add-weight calculation, bonus and deduction
 direct_reassign = {
-
+    "517370910xxx": 100,
+    "517370910xxy": 0
 }
+
+"""
+Below are some features that are nonsense now
+"""
+# """
+# How to decide the final submission time? Suppose you will define the time of the latest submission of three
+# problems as the final submission time of this assignment, write down the column of the used time (in seconds) of
+# three problems here
+# """
+# # Usually no need to fill this field if you are using from JOJ
+# csv_last_submission_timestamp_column = []
+
+# """
+# Date settings
+# """
+# # Usually no need to fill these field if you are using from JOJ
+#
+# start_date = datetime(2020, 9, 25)
+#
+# due_date = datetime(2020, 10, 11, 23, 59, 59)
+# # late deduction is multiplied to the full point of this assignment rather than the current score rather than,
+# # and then deducted from the current score
+# # late_deduction = [0.1, 0.1, 0.1] means 10% deduction per day, and 0 pt for more than three days
+# late_deduction = []
+#
+# # Bonus settings
+# has_bonus = False
+# bonus_date = datetime(2020, 10, 4, 23, 59, 59)
+# # bonus portion is multiplied to the current score rather than the full point of this assignment
+# bonus_portion = 0.1
